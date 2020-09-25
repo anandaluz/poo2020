8b0b6b9ae155a96d95843a864bdfefd0d8f74e94
anandaluz <48460523+anandaluz@users.noreply.github.com>
2020-09-24 21:22:35 -0300

    Add files via upload

---
+++ MotoA.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetomotoag;
+
+/**
+ *
+ * @author Thayn
+ */
+class MotoA {
+
+    void embarcar(String string, int idade) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void desembarcar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void dirigir() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
